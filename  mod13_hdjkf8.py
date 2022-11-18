a213db860eaca65b7aa07699b593fd97cb03c5c4
Hailey Jessee <107540588+HaileyDJessee@users.noreply.github.com>
2022-11-18 17:50:06 -0600

    Add files via upload

---
+++  mod13_hdjkf8.py
@@ -0,0 +1,19 @@
+print("Please enter a stock symbol (Capitalized, 1-7 Letters:")
+a = input()
+print(a)
+
+print("Please enter a chart type: For Bar please enter 1 and for Chart please enter 2")
+b = input()
+print(b)
+
+print("Please enter a time series (Select 1-4): Intraday = 1, Daily = 2, Weekly = 3, Monthly = 4")
+c = input()
+print(c)
+
+print("Please enter a Start date in the form YYYY-MM-DD")
+d = input()
+print(d)
+
+print("Please enter a End date in the form YYYY-MM-DD")
+e = input()
+print(e)
